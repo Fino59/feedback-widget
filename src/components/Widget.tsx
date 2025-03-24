c02e448f8a3769c44be3c9dedf27617fed2610ad
Fino59 <slipdan@hotmail.com>
2025-03-24 18:55:57 -0300

    chore: Changes to add WidgetForm popover

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,11 +1,19 @@
+// Icons
 import { ChatTeardropDots } from 'phosphor-react';
+
+// Libraries
 import { Popover, PopoverButton, PopoverPanel } from '@headlessui/react';
+
+// Components
+import { WidgetForm } from './WidgetForm';
 
 export function Widget() {
 
     return (
-        <Popover className='absolute bottom-4 right-4'>
-            <PopoverPanel><span>Hello</span></PopoverPanel>
+        <Popover className='absolute bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end'>
+            <PopoverPanel>
+                <WidgetForm />
+            </PopoverPanel>
 
             <PopoverButton className='bg-brand rounded-full px-3 h-12 text-white flex items-center group'>
                 <ChatTeardropDots className='w-6 h-6 ' />
